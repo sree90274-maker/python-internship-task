3160371c0dced051d5b809df11cafd18e6b90ea2
sree90274-maker <sree90274@gmail.com>
2026-05-25 07:19:42 +0530

    internshiptask1

---
+++ prediction.py
@@ -0,0 +1,45 @@
+#Weather Prediction using Logical Operators
+
+temperature = 30
+raining = True
+print("Take umbrella:", raining)
+print("Hot weather:", temperature > 35)
+print("Good weather:",temperature < 35 and not raining)
+
+#Data Type Conversion Tool
+
+num = "25"
+integer_num = int(num)
+float_num = float(integer_num)
+string_num = str(float_num)
+print("Integer:", integer_num)
+print("Float:", float_num)
+print("String:", string_num)
+
+# Compare Salaries of Employees
+
+employee1_salary = 40000
+employee2_salary = 50000
+print("Employee 1 Salary:", employee1_salary)
+print("Employee 2 Salary:", employee2_salary)
+print("Employee 1 salary is greater:",employee1_salary > employee2_salary)
+print("Employee 2 salary is greater:",employee2_salary > employee1_salary)
+
+#Create program to reverse words using slicing techniques.  
+text = "Python is very easy"
+print(text[::-1])
+
+
+print("...........")
+
+#Build a binary permission checker using bitwise operators.
+print(6&3)
+print(6|3)
+print(6^3)
+print(~3)
+print(6<<3)
+print(6>>3)
+
+ 
+
+
